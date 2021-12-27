b0482eaf8a0ab6cc01a2c148e45bbd448478616b
Jordan Corfield <JCorfield.1999@gmail.com>
2021-12-27 00:07:37 +0000

    Add Modal Sliders
    Add sliders to the modals so the user can move between the modals without having to close out of them.

--- js/employee_directory.js
+++ js/employee_directory.js
@@ -41,6 +41,8 @@ function createUserCard(employees) {
                 <div class="employee-modal">
                     <div class="modal-content">
                         <span class="close">&times;</span>
+                        <span class="slider prev">&lt;</span>
+                        <span class="slider next">&gt;</span>
                         <img src="${employee.picture.large}" alt="${employee.name.first} ${employee.name.last}">
                         <h2>${employee.name.first} ${employee.name.last}</h2>
                         <p>${employee.email}</p>
@@ -60,12 +62,34 @@ function viewModal(cards, modals) {
     for (let i = 0; i < cards.length; i++) {
         const card = cards[i];
         const modal = modals[i];
-        const close = modal.querySelector('.close');
+        const closeModal = modal.querySelector('.close');
+        const prevModal = modal.querySelector('.prev');
+        const nextModal = modal.querySelector('.next');
         card.addEventListener("click", () => {
             modal.style.display = "block";
         });
-        close.addEventListener("click", () => {
+        closeModal.addEventListener("click", () => {
             modal.style.display = "none";
+        });
+        prevModal.addEventListener("click", () => {
+            modal.style.display = "none";
+            let newModal = null;
+            if (i - 1 < 0) {
+                newModal = modals[modals.length - 1];
+            } else {
+                newModal = modals[i - 1];
+            }
+            newModal.style.display = "block";
+        });
+        nextModal.addEventListener("click", () => {
+            modal.style.display = "none";
+            let newModal = null;
+            if (i + 1 > modals.length - 1) {
+                newModal = modals[0];
+            } else {
+                newModal = modals[i + 1];
+            }
+            newModal.style.display = "block";
         });
     }
 }
@@ -80,7 +104,7 @@ function filter(employees, employeeCards) {
     searchBar.addEventListener("keyup", event => {
         const searchString = event.target.value;
         for (let i = 0; i < cards.length; i++) {
-            const card = cards[i].parentElement;
+            const card = cards[i];
             const name = names[i].toLowerCase();
             if (!name.startsWith(searchString.toLowerCase())) {
                 card.style.display = 'none';
